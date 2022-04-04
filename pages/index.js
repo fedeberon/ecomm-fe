cd14f693a5db1a16c6792355800a947345892ab1
Nicolas Basualdo Rey <95653230+NicobRey@users.noreply.github.com>
2022-04-04 14:15:00 -0300

    Index  Inicio getSSP

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import {getProducts} from "../services/productService";
 import Carrusel from "@/components/Carrusel";
 import * as brandsService from 'services/brandService';
 
+
 function IndexPage({products, brands}) {
   return (
       <>
@@ -11,17 +12,17 @@ function IndexPage({products, brands}) {
             <div className="flex container mx-auto min-h-full">
               <Carrusel/>
             </div>
-
-            <ProductListings products={products} brands={brands}/>
+            <ProductListings products={products} brands={brands} />
           </div>
         </div>
      </>
   )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const products = await getProducts();
   const brands = await brandsService.findAll();
+
    
   return {
     props: {
