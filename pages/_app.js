6d094d1c172d8d219320c87428ecb753cb98c832
fedeberon <federicoberon@outlook.com>
2022-04-28 09:17:50 -0300

    * Loading

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,16 @@ import Layout from '@/components/Layout'
 import SEO from '@/components/SEO'
 import '@/styles/globals.css'
 import { Provider } from 'next-auth/client'
+import NextNProgress from "nextjs-progressbar";
 
 export default function MyApp({ Component, pageProps }) {
 
           return (
               <Provider session={pageProps.session} >
                   <Layout>
-                    <SEO
-                      title={process.env.siteTitle}
-                    />
-                          <Component {...pageProps} />
+                      <SEO title={process.env.siteTitle}/>
+                      <NextNProgress />
+                      <Component {...pageProps} />
                   </Layout>
 
                 </Provider>
