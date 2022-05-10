1139d0b8dda6f1c6c1273378f0f5c2a1922613f3
Nicolas Basualdo Rey <95653230+NicobRey@users.noreply.github.com>
2022-05-10 14:12:14 -0300

    cambiosVarios(logo,verificar cuit)

--- pages/checkout/payment.js
+++ pages/checkout/payment.js
@@ -196,6 +196,7 @@ const Payment = ({user, myPoints, users}) => {
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 absolute right-1 top-2" fill="none" viewBox="0 0 24 24" stroke="red" stroke-width="2">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                         </svg>
+                                        
                                         :
                                         <></>
                                     }
