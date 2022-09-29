f73f01bf7e37cb4dabc20a0f0468495e08717bea
Nicolas Basualdo Rey <95653230+NicobRey@users.noreply.github.com>
2022-09-29 15:18:28 -0300

    Proveedores + color boton de filtro

---
+++ pages/proveedores/index.js
@@ -0,0 +1,36 @@
+import List from "@/components/proveedores/List";
+// import {getProducts} from "../../services/productService";
+import Link from "next/link";
+import PageTitle from "@/components/PageTitle";
+import { all } from "services/providersService";
+
+const ProductsManager = ({provider}) => {
+
+    return (
+
+        <div className="items-center mx-auto bg-white max-w-6xl">
+            <PageTitle text={`Proveedores`}/>
+            <List provider={provider}/>
+            <Link href="/proveedores/create" passHref>
+                <div className="w-full flex justify-center">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Nuevo Proveedor
+                    </button> 
+                </div>
+            </Link>
+        </div>
+    )
+
+}
+
+
+export async function getServerSideProps() {
+    const provider = await all()
+    return {
+        props: {
+            provider
+        },
+    }
+}
+
+export default ProductsManager
