13965ab54f31a24390fa5154e05e45bb175163c1
Tom <tomaslm_3@hotmail.co>
2023-01-02 11:35:16 -0300

    Minor change - Accessories

--- components/accessories/Category.js
+++ components/accessories/Category.js
@@ -1,8 +1,8 @@
 const Category = ({category, index}) => {
     return(
-        <div key={index} className="p-3 w-1/4 ">
-            <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-palette-primary rounded-lg shadow-md cursor-pointer ">
-                <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between hover:bg-palette-primary">
+        <div key={index} className="p-5">
+            <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 bg-palette-primary rounded-lg cursor-pointer ">
+                <div className="flex justify-evenly w-full h-full py-2 px-40 shadow-md bg-white rounded-lg transform duration-500 ease-in-out hover:bg-palette-primary hover:text-white">
                     <div className="my-auto">
                         <p className="font-bold">{category.name}</p>
                     </div>
