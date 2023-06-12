fcec0150297c8b24d611d16f396fad9505382f72
Leonardo <leonardo.couar@gmail.com>
2023-06-12 11:49:36 -0300

    deleted image where is admin

--- components/products/ProductImage.js
+++ components/products/ProductImage.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight , faTimes} from '@fortawesome/free-solid-svg-icons'
 import logo  from '../../images/default.jpeg'
+import {useSession} from "next-auth/client";
 import * as productService from 'services/productService'
 
 function ProductImage({ images , id}) {
@@ -16,6 +17,7 @@ function ProductImage({ images , id}) {
     const image = images && images.length != 0 ? images[0].link : defaultImage.link
     const [mainImg, setMainImg] = useState(image);
     const [delImg, setDelImg] = useState()
+    const [session, loading] = useSession();
     const ref = useRef();
 
     const [deletedModal,setDeletedModal] = useState(false)
@@ -78,7 +80,13 @@ function ProductImage({ images , id}) {
                       onClick={() => setMainImg(imgItem.link)}
                   />
                 }
-                <button className='absolute left-0' onClick={() => deleteProduct(imgItem)}><FontAwesomeIcon icon={faTimes} className="w-8 h-8 text-white bg-red-500 rounded-full p-1" /></button>
+                {
+                  session?.user?.role?.includes("ADMIN")
+                  ?
+                  <button className='absolute left-0' onClick={() => deleteProduct(imgItem)}><FontAwesomeIcon icon={faTimes} className="w-8 h-8 text-white bg-red-500 rounded-full p-1" /></button>
+                  :
+                  <></>
+                }
               </div>
             ))
           }
