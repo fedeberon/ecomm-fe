e2cddade280f3f4ddd0e58f0e1f587ae11c94ce8
fedeberon <federicoberon@outlook.com>
2023-06-15 17:33:28 -0300

    * Fix en el buscador

--- components/products/ProductListings.js
+++ components/products/ProductListings.js
@@ -1,70 +1,32 @@
-import ProductCard from '@/components/products/ProductCard'
-import {useEffect, useState} from "react";
-import {filterProductsByBrands, filterProductsByCategories, getProducts, search} from "../../services/productService"
-import BrandSearch from '../brands/BrandSearch';
-import CategorySearch from '../filter/CategorySearch';
+    import ProductCard from '@/components/products/ProductCard'
+    import {useEffect, useState} from "react";
+    import {filterProductsByBrands, filterProductsByCategories, getProducts, search} from "../../services/productService"
+    import BrandSearch from '../brands/BrandSearch';
+    import CategorySearch from '../filter/CategorySearch';
+    import { debounce } from 'lodash';
 
-function ProductListings({ products, brands, categories}) {
+    function ProductListings({ brands, categories}) {
 
     const [filter, isShowFilter] = useState(false)
     const [brandsToSearch, setBrandsToSearch] = useState([]);
     const [categoriesToSearch, setCategoriesToSearch] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [productsToShow, setProductsToShow] = useState()
+    const [searchTerm, setSearchTerm] = useState('');
+    const [productsToShow, setProductsToShow] = useState([])
+
+
     const [page, setPage] = useState(0);
 
-
-    let handleScroll = async (e) => {
-        if(window.innerHeight + e.target.documentElement.scrollTop + 1  > e.target.documentElement.scrollHeight && !isLoading) {
-          if(products.last===true){
-              return;
-          }
-          if (!productsToShow) {
-            let product = await getProducts(page)
-            setProductsToShow(product.content);
-            return; 
-          }
-          if (!products) {
-              let product = await getProducts(page)
-              setProductsToShow(product.content);
-              return;
-          }
-          if (brandsToSearch.length > 0 ) {
-              let product = await filterProductsByBrands(brandsToSearch) 
-              setProductsToShow(product);
-          } else if (categoriesToSearch.length > 0){
-              let product = await filterProductsByCategories(categoriesToSearch)
-              setProductsToShow(product)
-
-          } else {
-              setIsLoading(true)
-              let product = await getProducts(page + 1);
-              setPage(page + 1);
-              setProductsToShow(productsToShow => [
-                  ...productsToShow.concat (product.content)
-              ]);
-          }
-          setIsLoading(false)
-      }
+    const close = () => {
+     isShowFilter(false)
     }
 
-    useEffect( ()  => {
-        window.addEventListener('scroll', handleScroll);
+    const open = () => {
+     isShowFilter(!filter)
+    }
 
-        return(() => { window.removeEventListener('scroll', handleScroll) });
-    }, [products, handleScroll])
-
-  const close = () => {
-    isShowFilter(false)
-  }
-
-  const open = () => {
-    isShowFilter(!filter)
-  }
-
-  const handleChangeBrand = (e) => {
-
-      if(e.target.checked) {
+    const handleChangeBrand = (e) => {
+      if (e.target.checked) {
           setBrandsToSearch(brandsToSearch =>  [
               ...brandsToSearch,
               {
@@ -75,86 +37,120 @@ function ProductListings({ products, brands, categories}) {
           const brands = brandsToSearch.filter((brand) => brand.id !== e.target.value)
           setBrandsToSearch(brands);
       }
-  } 
-
-  const searchBrands = async () => {
-    const products = await filterProductsByBrands(brandsToSearch)
-    setProductsToShow(products);
-    close();
-  }
-
-  const handleChangeCategory = (e) => {
-
-    if(e.target.checked) {
-      
-        setCategoriesToSearch(categoriesToSearch =>  [
-            ...categoriesToSearch,
-            {
-                "id": e.target.value
-            }
-        ]);
-    } else {
-     
-        const categories = categoriesToSearch.filter((category) => category.id !== e.target.value)
-        setCategoriesToSearch(categories);
     }
-}
 
-const searchCategories = async () => {
-  const products =  await filterProductsByCategories(categoriesToSearch)
-  setProductsToShow(products)
-  close();
-}
-
-  const searchValue = async (e) => {
-    if(e.target.value.trim() === '') {
-      setProductsToShow(products)
-      
-      return;
-    }   
-    const result = await search(e.target.value);
-    if(result.length == 0) {
+    const searchBrands = async () => {
+        const products = await filterProductsByBrands(brandsToSearch)
+        setProductsToShow([])
         setProductsToShow(products);
-        return;
+        close();
     }
-    setProductsToShow(result); 
-  } 
 
-  const searchAll = () => {
-    searchCategories(); 
-    searchBrands();
-  }
-  // Get the button
-  const backToTopButton = () =>{
-  if (typeof window !== 'undefined') {
-    
-    const mybutton = document.getElementById("btn-back-to-top");
-    mybutton.addEventListener("click", backToTop);
-    window.onscroll = function () {
-    scrollFunction();
-  };
-  function scrollFunction() {
-            mybutton.style.display = "block";
-        }
-    // When the user clicks on the button, scroll to the top of the document
-    // When the user scrolls down 20px from the top of the document, show the button
-      
-      function backToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+    const handleChangeCategory = (e) => {
+      if (e.target.checked) {
+          setCategoriesToSearch(categoriesToSearch => [
+              ...categoriesToSearch,
+              {
+                  "id": e.target.value
+              }
+          ]);
+      } else {
+          const categories = categoriesToSearch.filter((category) => category.id !== e.target.value)
+          setCategoriesToSearch(categories);
       }
-      
-    } 
-}
-   return ( 
-          <div className='w-full'>
+    }
 
-                    
+    const searchCategories = async () => {
+      const products =  await filterProductsByCategories(categoriesToSearch)
+      setProductsToShow(products)
+      close();
+    }
+
+    const searchValue =  (valor) => {
+        search(valor).then((result) => {
+            setProductsToShow(result);
+        });
+    }
+
+    const searchAll = () => {
+        searchCategories();
+        searchBrands();
+    }
+
+    function backToTopButton() {
+        const button = document.createElement('button');
+        button.innerText = 'Volver arriba';
+        button.style.position = 'fixed';
+        button.style.bottom = '20px';
+        button.style.right = '20px';
+        button.style.display = 'none';
+
+        document.body.appendChild(button);
+
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset > 100) {
+                button.style.display = 'block';
+            } else {
+                button.style.display = 'none';
+            }
+        });
+
+        button.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+    }
+
+    let handleScroll = async (e) => {
+        if(window.innerHeight + e.target.documentElement.scrollTop + 1  > e.target.documentElement.scrollHeight && !isLoading) {
+            if (brandsToSearch.length > 0 ) {
+                let product = await filterProductsByBrands(brandsToSearch)
+                setProductsToShow(product);
+            } else if (categoriesToSearch.length > 0){
+                let product = await filterProductsByCategories(categoriesToSearch)
+                setProductsToShow(product)
+            } else if (document.getElementById("search").value !== '') {
+                return;
+            } else {
+                setIsLoading(true)
+                let result = await getProducts(page + 1);
+                setPage(page + 1);
+                setProductsToShow(productsToShow => [
+                    ...productsToShow.concat (result.content)
+                ]);
+            }
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        const debouncedSearch = debounce(searchValue, 500); // Retraso de 500 ms
+
+        debouncedSearch(searchTerm);
+
+        return () => {
+            debouncedSearch.cancel(); // Cancela la ejecución del debouncedSearch al desmontar el componente
+        };
+    }, [searchTerm]);
+
+    function handleChange(event) {
+        const { value } = event.target;
+        setSearchTerm(value);
+    }
+
+    useEffect( ()  => {
+        window.addEventListener('scroll', handleScroll);
+
+        return(() => { window.removeEventListener('scroll', handleScroll) });
+    }, [handleScroll])
+
+        return (
+          <div className='w-full'>
                 <div className=' sticky top-14 z-30 bg-white'>
                     <div className='flex justify-center py-2 h-20 '>
-
                         <div className='justify-between my-auto mx-4'>
-                            
                             <button className="text-white  bg-palette-secondary border border-solid border-palette-secondary hover:bg-palette-slight hover:text-white active:bg-palette-slight font-bold
                                             uppercase
                                             text-xl
@@ -168,15 +164,17 @@ const searchCategories = async () => {
                                             transition-all
                                             duration-150"
                                             type="button"
-                                            onClick={open}>     
+                                            onClick={open}>
                             {filter ? "Cerrar" : "Filtros" }
                             </button>
                         </div>
-
-                                <input
-                                    className="w-2/3 text-palette-secondary border border-solid border-palette-secondary placeholder-palette-slighter font-semibold text-xl p-2 my-auto rounded-xl shadow-lg shadow-indigo-500/50 outline-none transition-all"
-                                    placeholder="Buscar"
-                                    onChange={searchValue}/>
+                        <input
+                            className="w-2/3 text-palette-secondary border border-solid border-palette-secondary placeholder-palette-slighter font-semibold text-xl p-2 my-auto rounded-xl shadow-lg shadow-indigo-500/50 outline-none transition-all"
+                            placeholder="Buscar"
+                            id="search"
+                            value={searchTerm}
+                            onChange={handleChange}
+                            autoComplete="off"/>
                       </div>
                     <div className={`fixed z-50  top-0 w-full left-0 ${filter ? "" : "hidden"}  `} id="modal">
                         <div className="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
@@ -203,35 +201,31 @@ const searchCategories = async () => {
 
 
               <div className="mx-auto mt-3 w-11/12">
-                  <div className="grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-9 2xl:gap-4 ">
-                      {
-                          productsToShow
-                          ?
-                          productsToShow.map((product, index) => (
+                    <div className="grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-9 2xl:gap-4 ">
+                        {
+                            productsToShow.map((product, index) => (
                               <ProductCard key={index} product={product} />
                           ))
-                          :
-                          <></>
-                      }
-          </div>
-          <button type="button" data-mdb-ripple="true" onMouseDown={backToTopButton} data-mdb-ripple-color="light" className="z-0 -mx-9 md:-mx-7 shadow-lg invisible md:visible ease-out duration-500 sticky p-2 bg-palette-secondary animate-bounce text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-palette-sdark hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg bottom-5 right-2" id="btn-back-to-top">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-5 h-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
-          </button>
-                  </div>
+                        }
+                    </div>
+                    <button type="button" data-mdb-ripple="true" onMouseDown={backToTopButton} data-mdb-ripple-color="light" className="z-0 -mx-9 md:-mx-7 shadow-lg invisible md:visible ease-out duration-500 sticky p-2 bg-palette-secondary animate-bounce text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-palette-sdark hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg bottom-5 right-2" id="btn-back-to-top">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-5 h-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
+                    </button>
+              </div>
 
               {
-                isLoading 
+                isLoading
                 ?
-                <div className='flex items-center justify-center py-6'>
-                            <div className='w-16 h-16 border-b-2 border-palette-secondary rounded-full animate-spin'></div>
-                        </div> 
-                        : 
-                        <></>
-                      }
-              
+               <div className='flex items-center justify-center py-6'>
+                    <div className='w-16 h-16 border-b-2 border-palette-secondary rounded-full animate-spin'></div>
+                </div>
+                :
+                <></>
+              }
+
 
               </div>
-  )
-}
+    )
+    }
 
-export default ProductListings
+    export default ProductListings
