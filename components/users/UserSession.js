5b5d9130a55e3438b3a36e9221c7f9e811add479
Alan Medina <medinaalangabriel@gmail.com>
2023-08-22 16:42:15 -0300

    Centrado de boton Categorias y en sesión.

--- components/users/UserSession.js
+++ components/users/UserSession.js
@@ -25,7 +25,7 @@ const UserSession = ({session}) => {
                                     id="menu-button" aria-expanded="true" aria-haspopup="true">
                                 {(session.user.name).toUpperCase()}
                                 <svg className="h-5 mt-1 w-5" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                     viewBox="0 3 20 20" fill="currentColor" aria-hidden="true">
                                     <path fillRule="evenodd"
                                           d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                                           clipRule="evenodd"/>
