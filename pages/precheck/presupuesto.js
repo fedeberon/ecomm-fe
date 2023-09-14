e647b2d47e31ff94669364196dba007ba7c2f968
EnzoCepeda <enzoocobhc@hotmail.com.ar>
2023-09-14 14:55:18 -0300

    fix PrecheckPrint

--- pages/precheck/presupuesto.js
+++ pages/precheck/presupuesto.js
@@ -1,20 +1,20 @@
-import {useEffect, useRef, useState} from "react";
-import {useCartContext, useCleanCartContext} from '@/context/Store'
-import {getSession} from "next-auth/client";
-import {getPoints} from "../../services/walletService";
-import {findAll, getByUsername} from "../../services/userService";
-import {NotificationContainer, NotificationManager} from 'react-notifications';
+import { useEffect, useRef, useState } from "react";
+import { useCartContext, useCleanCartContext } from '@/context/Store'
+import { getSession } from "next-auth/client";
+import { getPoints } from "../../services/walletService";
+import { findAll, getByUsername } from "../../services/userService";
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 import PageTitle from '@/components/PageTitle'
 import Link from "next/link";
-import {useReactToPrint} from "react-to-print";
-import {buyWithPoints, createCheckout} from "../../services/productService";
+import { useReactToPrint } from "react-to-print";
+import { buyWithPoints, createCheckout } from "../../services/productService";
 import Loading from "@/components/utils/Loading";
 import PrecheckPrint from "@/components/bill/PrecheckPrint";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 
-const Presupuesto = ({userSession, users}) => {
+const Presupuesto = ({ userSession, users }) => {
     const [checkout, setCheckout] = useState();
     const [cart, checkoutUrl] = useCartContext()
     const cleanCart = useCleanCartContext()
@@ -27,10 +27,10 @@ const Presupuesto = ({userSession, users}) => {
         setLoading(true);
         let checkout = await createCheckout(cart);
         setCheckout(checkout.data);
-        cleanCart();
+        // cleanCart();
+        console.log(checkout.data);
         setLoading(false);
     }, []);
-
     const componentRed = useRef()
     const print = useReactToPrint({
         content: () => componentRed.current,
@@ -42,13 +42,13 @@ const Presupuesto = ({userSession, users}) => {
         "username": "",
         "name": "",
         "lastName": "",
-        "address" : "",
+        "address": "",
         "cuit": ""
     })
 
 
     const handleChangeUsers = (e) => {
-        const {value} = e.target;
+        const { value } = e.target;
         getPoints(value).then((res) => {
             setPoints(res)
         })
@@ -88,44 +88,45 @@ const Presupuesto = ({userSession, users}) => {
         }
     }
 
-    async function deleteCart () {
+    async function deleteCart() {
         NotificationManager.info('Carrito vaciado', () => {
             router.push('/')
-          });
-          cleanCart();
+        });
+        cleanCart();
     }
 
-    async function checkSubmit () {
+    async function checkSubmit() {
         NotificationManager.info('Compra realizada con exito', () => {
             router.push('/')
-          });
-          window.location.href = "/"
-          cleanCart();
+        });
+        window.location.href = "/"
+        cleanCart();
     }
 
-    return(
+    return (
         <>
-        <NotificationContainer/>
+
+            <NotificationContainer />
+
             {
                 loading
                     ?
                     <Loading message={"Un momento por favor ..."} />
                     :
                     <div>
-                        <div id="presupuesto">
+                        <div id="presupuesto"  >
                             <div>
                                 <PageTitle text={`Presupuesto # ${checkout.id}`} />
                             </div>
                             {
-                                userSession?.role?.includes("ADMIN")
-                                    ?
-                                    <div className="m-auto w-1/2">
-                                        <select
-                                            id="user"
-                                            className="text-gray-600 focus:outline-none  font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
-                                            onChange={handleChangeUsers}
-                                        >
-                                            <option value="seleccionar">Seleccione el usuario </option>
+                            userSession?.role?.includes("ADMIN")
+                            ?
+                            <div className="m-auto w-1/2">
+                                <select
+                                    id="user"
+                                    className="text-gray-600 focus:outline-none  font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+                                    onChange={handleChangeUsers}>
+                                        <option value="seleccionar">Seleccione el usuario </option>
                                             {users.map((user, index) => {
                                                 return (
                                                     <option
@@ -137,16 +138,16 @@ const Presupuesto = ({userSession, users}) => {
                                                     </option>
                                                 );
                                             })}
-                                        </select>
-                                    </div>
-                                    :
-                                    <></>
-
+                                </select>
+                            </div>
+                            :
+                            <>
+                            </>
                             }
 
-                            <div ref={componentRed} className="max-h-96">
-                                <PrecheckPrint checkout={checkout}/>
-                            </div>
+                        <div ref={componentRed} className="">
+                            <PrecheckPrint checkout={checkout} />
+                        </div>
 
                         </div>
                         <div className="flex w-full justify-center">
@@ -158,50 +159,60 @@ const Presupuesto = ({userSession, users}) => {
                             <button onClick={print} type="button" className="md:m-3 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
                                 Imprimir
                             </button>
+                            <p aria-label="checkout-products"
+                                className="md:m-3 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                SELECCIONAR USUARIO
+                            </p>
+                        </div>
 
-                                {
-                                    !personLoaded
+
+
+                        {
+                            !personLoaded
+                                ?
+                                (
+                                    <>
+                                    </>
+                                )
+                                :
+                                (
+                                    person.twins
                                         ?
                                         (
-                                            <p  aria-label="checkout-products"
-                                                className="md:m-3 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                                                SELECCIONAR USUARIO
+                                            <p
+                                                aria-label="checkout-products"
+                                                className="md:m-3 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                                            >
+                                                EL 20% DE DESCUENTO SE APLICARA A EN LA FACTURA FINAL
+                                                DEL TOTAL DE LA COMPRA
+
+
                                             </p>
-                                        )
-                                        :
+                                        ) :
                                         (
-                                            person.twins
-                                                ?
-                                                (
-                                                    <p
-                                                        aria-label="checkout-products"
-                                                        className="md:m-3 justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-                                                    >
-                                                        EL 20% DE DESCUENTO SE APLICARA A EN LA FACTURA FINAL
-                                                        DEL TOTAL DE LA COMPRA
-
-
-                                                    </p>
-                                                ) :
-                                                (
-                                                    <a  onClick={() => handleCreditPoints(person.username)}
-                                                        value={person}
-                                                        aria-label="checkout-products"
-                                                        className="md:m-3 justify-center bg-gradient-to-r from-blue-900 to-blue-500 rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                                                        Tarjeta de Puntos. Saldo: {points}
-                                                    </a>
-                                                )
+                                            <a onClick={() => handleCreditPoints(person.username)}
+                                                value={person}
+                                                aria-label="checkout-products"
+                                                className="md:m-3 justify-center bg-gradient-to-r from-blue-900 to-blue-500 rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                                Tarjeta de Puntos. Saldo: {points}
+                                            </a>
                                         )
-                                }
+                                )
+                        }
 
 
-                        </div>
                     </div>
+
             }
+
         </>
+
+
     )
+
+
 }
- 
+
 
 export default Presupuesto;
 
@@ -209,13 +220,13 @@ export default Presupuesto;
 export async function getServerSideProps(context) {
     const session = await getSession(context)
     const users = await findAll();
-    if(session == null) {
+    if (session == null) {
         return {
             redirect: {
                 permanent: false,
                 destination: "/login",
             },
-            props:{},
+            props: {},
         };
     }
     const myPoints = await getPoints(session.user.username);
