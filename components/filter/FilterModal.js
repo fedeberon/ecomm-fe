445546f2a61eb6c645c7fc2ca1d829414a7a5c0b
Alan Medina <medinaalangabriel@gmail.com>
2023-09-25 17:56:36 -0300

    En proceso (No funcional aun) 25/9

--- components/filter/FilterModal.js
+++ components/filter/FilterModal.js
@@ -1,6 +1,127 @@
 //Componente nuevo a definir a futuro
-function FilterModal(){
-    return null;
+import { useEffect, useState } from "react";
+import BrandSearch from '../brands/BrandSearch';        //A ELIMINAR, SERAN UNA FUNCION PROPIA Y UNICA
+import CategorySearch from '../filter/CategorySearch';  //A ELIMINAR, SERAN UNA FUNCION PROPIA Y UNICA
+import { debounce } from 'lodash';
+
+function FilterModal({ clasifications, searchFunction }) {
+    const [filter, isShowFilter] = useState(false)
+    //¿DEBERIA HABER UN ARRAY MULTIPLE? ¿UN ARREGLO DENTADO?
+    const [brandsToSearch, setBrandsToSearch] = useState([]);
+    const [categoriesToSearch, setCategoriesToSearch] = useState([]);
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    //PERTENECERA A NUEVO COMPONENTE
+    const close = () => {
+        isShowFilter(false)
+    }
+
+    //PERTENECERA A NUEVO COMPONENTE
+    const open = () => {
+        isShowFilter(!filter)
+    }
+
+    //PERTENECERA A NUEVO COMPONENTE - A UNIFICAR CON SIGUIENTE
+    const handleChangeBrand = (e) => {
+        if (e.target.checked) {
+            setBrandsToSearch(brandsToSearch => [
+                ...brandsToSearch,
+                {
+                    "id": e.target.value
+                }
+            ]);
+        } else {
+            const brands = brandsToSearch.filter((brand) => brand.id !== e.target.value)
+            setBrandsToSearch(brands);
+        }
+    }
+
+    //PERTENECERA A NUEVO COMPONENTE - A UNIFICAR CON ANTERIOR
+    const handleChangeCategory = (e) => {
+        if (e.target.checked) {
+            setCategoriesToSearch(categoriesToSearch => [
+                ...categoriesToSearch,
+                {
+                    "id": e.target.value
+                }
+            ]);
+        } else {
+            const categories = categoriesToSearch.filter((category) => category.id !== e.target.value)
+            setCategoriesToSearch(categories);
+        }
+    }
+
+    //PERTENECERA A NUEVO COMPONENTE    
+    useEffect(() => {
+        const debouncedSearch = debounce(searchValue, 500); // Retraso de 500 ms
+        debouncedSearch(searchTerm);
+
+        return () => {
+            debouncedSearch.cancel(); // Cancela la ejecución del debouncedSearch al desmontar el componente
+        };
+    }, [searchTerm]);
+
+    //PERTENECERA A NUEVO COMPONENTE    
+    function handleChange(event) {
+        const { value } = event.target;
+        setSearchTerm(value);
+    }
+
+    return (
+        <div className=' sticky top-16 pt-4 md:top-14 md:pt-0 z-30 bg-white'>
+            <div className='flex justify-center py-2 h-20 '>
+                <div className='justify-between my-auto mx-4'>
+                    <button className="text-white  bg-palette-secondary border 
+                border-solid border-palette-secondary hover:bg-palette-slight 
+                hover:text-white active:bg-palette-slight font-bold
+                                uppercase
+                                text-xl
+                                p-2
+                                my-auto
+                                rounded-xl
+                                shadow-lg shadow-indigo-500/50
+                                outline-none
+                                focus:outline-none
+                                ease-linear
+                                transition-all
+                                duration-150"
+                        type="button"
+                        onClick={open}>
+                        Filtros
+                    </button>
+                </div>
+                <input
+                    className="w-2/3 text-palette-secondary border border-solid border-palette-secondary placeholder-palette-slighter font-semibold text-xl p-2 my-auto rounded-xl shadow-lg shadow-indigo-500/50 outline-none transition-all"
+                    placeholder="Buscar"
+                    id="search"
+                    value={searchTerm}
+                    onChange={handleChange}
+                    autoComplete="off" />
+            </div>
+            <div className={`fixed z-50  top-0 w-full left-0 ${filter ? "" : "hidden"}  `} id="modal">
+                <div className="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                    <div onClick={close} className="fixed inset-0 transition-opacity">
+                        <div className="absolute inset-0 bg-gray-700 opacity-75" />
+                    </div>
+                    <span className=" sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
+                    <div className="w-auto inline-block bg-white  rounded-lg text-left  shadow-xl transform transition-all my-8 align-middle"
+                        role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                        <div className='absolute mt-20 w-full border-b-2 border-indigo-100 '></div>
+                        <div className="flex grid-cols-2 m-auto px-4 pt-6 pb-2 sm:p-6 sm:pb-4">
+                            {/* A MAPEAR */}
+
+                            {/* A MAPEAR */}
+                            <div className="p-3  mt-2 text-center space-x-4 md:block">
+                                <button className="mb-2 md:mb-0 bg-palette-slight border border-black-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white hover:text-white rounded-full hover:shadow-lg hover:bg-palette-secondary" onClick={close}>Cerrar</button>
+                                <button className="mb-2 md:mb-0 bg-palette-slight border-black-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white hover:text-white rounded-full hover:shadow-lg hover:bg-palette-secondary" onClick={searchFunction}>Buscar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default FilterModal;
--- components/products/ProductListings.js
+++ components/products/ProductListings.js
@@ -1,59 +1,12 @@
 import ProductCard from '@/components/products/ProductCard'
+import FilterModal from '@/components/filter/FilterModal'
 import { useEffect, useState } from "react";
 import { filterProductsByBrands, filterProductsByCategories, getProducts, search } from "../../services/productService"
-import BrandSearch from '../brands/BrandSearch';
-import CategorySearch from '../filter/CategorySearch';
-import { debounce } from 'lodash';
 
 function ProductListings({ brands, categories }) {
-
-    const [filter, isShowFilter] = useState(false)
-    const [brandsToSearch, setBrandsToSearch] = useState([]);
-    const [categoriesToSearch, setCategoriesToSearch] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
     const [productsToShow, setProductsToShow] = useState([])
     const [page, setPage] = useState(0);
-
-    //PERTENECERA A NUEVO COMPONENTE
-    const close = () => {
-        isShowFilter(false)
-    }
-
-    //PERTENECERA A NUEVO COMPONENTE
-    const open = () => {
-        isShowFilter(!filter)
-    }
-
-    //PERTENECERA A NUEVO COMPONENTE - A UNIFICAR CON SIGUIENTE
-    const handleChangeBrand = (e) => {
-        if (e.target.checked) {
-            setBrandsToSearch(brandsToSearch => [
-                ...brandsToSearch,
-                {
-                    "id": e.target.value
-                }
-            ]);
-        } else {
-            const brands = brandsToSearch.filter((brand) => brand.id !== e.target.value)
-            setBrandsToSearch(brands);
-        }
-    }
-
-    //PERTENECERA A NUEVO COMPONENTE - A UNIFICAR CON ANTERIOR
-    const handleChangeCategory = (e) => {
-        if (e.target.checked) {
-            setCategoriesToSearch(categoriesToSearch => [
-                ...categoriesToSearch,
-                {
-                    "id": e.target.value
-                }
-            ]);
-        } else {
-            const categories = categoriesToSearch.filter((category) => category.id !== e.target.value)
-            setCategoriesToSearch(categories);
-        }
-    }
 
     //A ELIMINAR - SEARCHALL DEBERA REEMPLAZARLO
     const searchBrands = async () => {
@@ -69,6 +22,7 @@ function ProductListings({ brands, categories }) {
         close();
     }
 
+    //A ELIMINAR - SEARCHALL DEBERA REEMPLAZARLO
     const searchValue = (valor) => {
         search(valor).then((result) => {
             setProductsToShow(result);
@@ -76,6 +30,7 @@ function ProductListings({ brands, categories }) {
     }
 
     //SERA LA UNICA FUNCION PARA BUSQUEDA PARA PRODUCTOS
+    //LA FUNCION ESPECIFICA DE BUSQUEDA DEBERA SER PASADA COMO PARAMETRO PARA FILTERMODAL...
     const searchAll = () => {
         searchCategories();
         searchBrands();
@@ -113,22 +68,6 @@ function ProductListings({ brands, categories }) {
         }
     }
 
-    //PERTENECERA A NUEVO COMPONENTE    
-    useEffect(() => {
-        const debouncedSearch = debounce(searchValue, 500); // Retraso de 500 ms
-        debouncedSearch(searchTerm);
-
-        return () => {
-            debouncedSearch.cancel(); // Cancela la ejecución del debouncedSearch al desmontar el componente
-        };
-    }, [searchTerm]);
-
-    //PERTENECERA A NUEVO COMPONENTE    
-    function handleChange(event) {
-        const { value } = event.target;
-        setSearchTerm(value);
-    }
-
     //PROPIA DE PRODUCTLISTINGS
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
@@ -136,62 +75,12 @@ function ProductListings({ brands, categories }) {
         return (() => { window.removeEventListener('scroll', handleScroll) });
     }, [handleScroll])
 
-    return (
+    return (        
         <div className='w-full'>
-            <div className=' sticky top-16 pt-4 md:top-14 md:pt-0 z-30 bg-white'>
-                <div className='flex justify-center py-2 h-20 '>
-                    <div className='justify-between my-auto mx-4'>
-                        <button className="text-white  bg-palette-secondary border 
-                            border-solid border-palette-secondary hover:bg-palette-slight 
-                            hover:text-white active:bg-palette-slight font-bold
-                                            uppercase
-                                            text-xl
-                                            p-2
-                                            my-auto
-                                            rounded-xl
-                                            shadow-lg shadow-indigo-500/50
-                                            outline-none
-                                            focus:outline-none
-                                            ease-linear
-                                            transition-all
-                                            duration-150"
-                            type="button"
-                            onClick={open}>
-                            Filtros
-                        </button>
-                    </div>
-                    <input
-                        className="w-2/3 text-palette-secondary border border-solid border-palette-secondary placeholder-palette-slighter font-semibold text-xl p-2 my-auto rounded-xl shadow-lg shadow-indigo-500/50 outline-none transition-all"
-                        placeholder="Buscar"
-                        id="search"
-                        value={searchTerm}
-                        onChange={handleChange}
-                        autoComplete="off" />
-                </div>
-                <div className={`fixed z-50  top-0 w-full left-0 ${filter ? "" : "hidden"}  `} id="modal">
-                    <div className="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                        <div onClick={close} className="fixed inset-0 transition-opacity">
-                            <div className="absolute inset-0 bg-gray-700 opacity-75" />
-                        </div>
-                        <span className=" sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
-                        <div className="w-auto inline-block bg-white  rounded-lg text-left  shadow-xl transform transition-all my-8 align-middle"
-                            role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-                            <div className='absolute mt-20 w-full border-b-2 border-indigo-100 '></div>
-                            <div className="flex grid-cols-2 m-auto px-4 pt-6 pb-2 sm:p-6 sm:pb-4">
-                                <CategorySearch categories={categories} onclick={handleChangeCategory} />
-                                <BrandSearch brands={brands} onclick={handleChangeBrand} />
-                            </div>
-
-                            <div className="p-3  mt-2 text-center space-x-4 md:block">
-                                <button className="mb-2 md:mb-0 bg-palette-slight border border-black-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white hover:text-white rounded-full hover:shadow-lg hover:bg-palette-secondary" onClick={close}>Cerrar</button>
-                                <button className="mb-2 md:mb-0 bg-palette-slight border-black-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white hover:text-white rounded-full hover:shadow-lg hover:bg-palette-secondary" onClick={searchAll}>Buscar</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
+            <FilterModal 
+                clasifications={[brands, categories]} 
+                searchFunction={searchAll}>
+            </FilterModal>
             <div className="mx-auto mt-3 w-11/12">
                 <div className="grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-9 2xl:gap-4 ">
                     {
