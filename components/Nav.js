d0ecd38fce8a6587c2ac1524a1b5c004d5123feb
EnzoCepeda <enzoocobhc@hotmail.com.ar>
2023-09-25 17:31:51 -0300

    FixCategoriesList

--- components/Nav.js
+++ components/Nav.js
@@ -122,9 +122,9 @@ function Nav() {
                   </svg>
               </button>       
               <div
-                className={`${categoriesVisible ? "" : "hidden"} z-50 absolute mt-2 w-46 lg:w-32 lg:right-0 rounded-md shadow-lg bg-white ring-2 ring-palette-lighter ring-opacity-75 focus:outline-none md:-mx-2 -mx-0`}
+                className={`${categoriesVisible ? "" : "hidden"} z-50 absolute mt-2 w-46 lg:w-32 lg:right-0 md:w-32 rounded-md shadow-lg bg-white ring-2 ring-palette-lighter ring-opacity-75 focus:outline-none md:-mx-2 -mx-0`}
                 role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
-                <div className="overflow-y-auto no-scrollbar h-80" role="none">
+                <div className="overflow-y-auto no-scrollbar max-h-80 lg:max-h-44" role="none">
                   {categories?.map((category) =>(
                       <Link href={`/accessories/${category.id}`} passHref legacyBehavior>
                         <a href="#" onClick={showCategories} className="text-palette-primary block text-center hover:text-palette-secondary px-4 py-2 text-sm" role="menuitem"
