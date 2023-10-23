40452d3362c5f8a34bf5b2602816f6ffae2416eb
Alan Medina <medinaalangabriel@gmail.com>
2023-10-23 17:15:56 -0300

    Fix menor - No es necesario hacer clic  2 veces

--- components/mercadoPago/MercadoPago.js
+++ components/mercadoPago/MercadoPago.js
@@ -1,23 +1,14 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faShoppingBag} from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 
-const MercadoPago = ({preference}) => {
+const MercadoPago = ({ preference }) => {
 
+    useEffect(() => {
+        window.location.href = preference;
+    }, [preference]);
 
-
-    return (
-        <>
-            <a href={preference}
-               aria-label="back-to-products"
-               className="border border-palette-primary text-lg font-primary font-semibold pt-2 pb-1 leading-relaxed flex
-               bg-blue-400 text-white
-                justify-center items-center focus:ring-1 focus:ring-palette-light focus:outline-none w-full rounded-sm"
-            >
-                <FontAwesomeIcon icon={faShoppingBag} className="w-4 mr-2 inline-flex" />
-
-            Pagar con Mercado Pago</a>
-        </>
-    )
+    return null;
 }
 
 export default MercadoPago;
