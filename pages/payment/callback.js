ce5a2fa03bb13952905a8f36e9e1ba3ebee54541
Alan Medina <medinaalangabriel@gmail.com>
2023-10-23 14:11:53 -0300

    Callback devuelve al checkout correcto.

--- pages/payment/callback.js
+++ pages/payment/callback.js
@@ -1,26 +1,39 @@
-import {callbackPayment} from "../../services/productService";
-import { useCleanCartContext } from '@/context/Store'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { callbackPayment } from "../../services/productService";
+import { useCartContext, useCleanCartContext } from '@/context/Store';
 
-const emptyCart = useCleanCartContext();
-const Callback = () => {
-    return (
-        <></>
-    )
-}
+const Callback = ({ data }) => {
+    const [cart, checkoutUrl] = useCartContext();
+    const cleanCart = useCleanCartContext();
+    const [redirect, setRedirect] = useState(false);
+    const router = useRouter();
 
+    useEffect(() => {
+        const handleCleanCart = async () => {
+            if (data.checkoutState !== "REJECTED") await cleanCart();
+            setRedirect(true);
+        };
+        handleCleanCart();
+        if (redirect) {
+            router.push('/checkout/' + data.checkoutId);
+        }
+    }, [redirect]);
+
+    // Your JSX rendering code here
+    return <></>;
+};
 
 export default Callback;
 
-export async function getServerSideProps({query}) {
+export async function getServerSideProps({ query }) {
+    // Fetch the data from the server
     const data = await callbackPayment(query);
 
-    if(data.checkoutState != "REJECTED") emptyCart();
-
+    // Return the data as props
     return {
-        redirect: {
-            destination: '/checkout/' + data.checkoutId,
-            permanent: false,
+        props: {
+            data,
         },
-    }
-
+    };
 }
