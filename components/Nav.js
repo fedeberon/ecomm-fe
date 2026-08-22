f22c1145c9878aeb2d7f74555696191be69ddc13
federico beron <federicoberon@outlook.com>
2026-08-22 15:42:56 -0300

    Usar asset corregido del logo

--- components/Nav.js
+++ components/Nav.js
@@ -42,7 +42,7 @@ function Nav() {
             </div>
             <Link href="/">
               <a className="absolute left-1/2 -translate-x-1/2 top-0 lg:-top-7 z-20 flex items-center justify-center w-24 h-24 lg:w-32 lg:h-32">
-                <img src="/images/logo-dulce-bebe.svg" className="w-full h-full object-contain drop-shadow-[0_10px_18px_rgba(15,23,42,0.18)]" alt="Dulce Bebé" />
+                <img src="/images/logo-dulce-bebe-fixed.svg" className="w-full h-full object-contain drop-shadow-[0_10px_18px_rgba(15,23,42,0.18)]" alt="Dulce Bebé" />
               </a>
             </Link>
             <div className="flex items-center gap-2 lg:gap-6 ml-auto">
