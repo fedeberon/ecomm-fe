4f76bde62839b1ea022880dac78a8bf7f1b891d1
federico beron <federicoberon@outlook.com>
2026-08-22 15:56:25 -0300

    Integrar nueva ficha de detalle del producto

--- components/products/ProductDetails.js
+++ components/products/ProductDetails.js
@@ -3,37 +3,29 @@ import ProductForm from '@/components/products/ProductForm'
 import logo from "../../images/default.jpeg";
 import BackToProductButton from './BackToProductButton';
 
+function ProductDetails({ productData }) {
+  const defaultImage = {
+    url: "default.jpeg",
+    link: logo,
+    main: false
+  };
 
-function ProductDetails({ productData}) {
-    const defaultImage =
-        {
-            "url": "default.jpeg",
-            "link": logo,
-            "main": false
-        };
-        true
-   const image = productData.images && productData.images.length != 0 ? productData.images[0].link : defaultImage.link
+  const image = productData.images && productData.images.length !== 0
+    ? productData.images[0].link
+    : defaultImage.link
 
   return (
+    <div className="flex flex-col h-full">
+      <ProductInfo productData={productData} />
 
-    <div className="flex py-1 flex-col justify-between w-full max-w-xs mx-auto space-y-4 min-h-128">  
-      <div>
-        <ProductInfo 
-          title={productData.name}
-          description={productData.description}
-          price={productData.price}
-        />
+      <div className="mt-7 pt-6 border-t border-slate-100">
+        <ProductForm productData={productData} image={image} />
       </div>
 
-      <ProductForm 
-        productData={productData}
-        image={image}
-      />
-      <div className=''>
+      <div className="mt-4">
         <BackToProductButton />
       </div>
     </div>
-    
   )
 }
 
