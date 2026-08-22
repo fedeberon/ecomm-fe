b34fbaf2c33d1ed0bc85717f8c2f1d6dfd49355b
federico beron <federicoberon@outlook.com>
2026-08-22 15:33:21 -0300

    Usar nuevo logo generado en header

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { useCartContext } from "@/context/Store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faShoppingCart, faHome } from "@fortawesome/free-solid-svg-icons";
-import logo from "/images/logoMati.png";
 import UserSession from "@/components/users/UserSession";
 import { useSession } from "next-auth/client";
 import { findAll } from "services/categoriesService";
@@ -41,7 +40,11 @@ function Nav() {
               <Link href="/"><a className="hidden lg:flex items-center gap-2 py-3 text-sm font-bold text-gray-800 hover:text-palette-sdark"><FontAwesomeIcon icon={faHome} className="w-4 text-palette-sdark" />Inicio</a></Link>
               <Link href="/diapers/inicio"><a className="hidden lg:block py-3 text-sm font-bold text-gray-800 hover:text-palette-sdark">Pañalería</a></Link>
             </div>
-            <Link href="/"><a className="absolute left-1/2 -translate-x-1/2 top-0 lg:-top-7 z-20 flex items-center justify-center w-24 h-24 lg:w-36 lg:h-36 rounded-full bg-white shadow-xl border border-gray-100 p-2"><img src={logo.src} className="w-full h-full object-contain" alt="Dulce Bebé" /></a></Link>
+            <Link href="/">
+              <a className="absolute left-1/2 -translate-x-1/2 top-0 lg:-top-7 z-20 flex items-center justify-center w-24 h-24 lg:w-32 lg:h-32">
+                <img src="/images/logo-dulce-bebe.svg" className="w-full h-full object-contain drop-shadow-[0_10px_18px_rgba(15,23,42,0.18)]" alt="Dulce Bebé" />
+              </a>
+            </Link>
             <div className="flex items-center gap-2 lg:gap-6 ml-auto">
               <div className="hidden lg:block relative">
                 <button type="button" onClick={() => setCategoriesVisible(!categoriesVisible)} className="flex items-center gap-1 py-3 text-sm font-bold text-gray-800 hover:text-palette-sdark">Categorías <span className="text-xs">⌄</span></button>
