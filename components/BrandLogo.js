ce1c5e0f35ced89cbf345ce85a3784c9a45c10ea
federico beron <federicoberon@outlook.com>
2026-08-22 22:02:55 -0300

    fix: switch header to Dulce Bebe logo v6

--- components/BrandLogo.js
+++ components/BrandLogo.js
@@ -1,8 +1,8 @@
 function BrandLogo() {
   return (
     <img
-      src="/images/dulce-bebe-logo-v5.svg"
-      className="h-14 sm:h-16 lg:h-20 w-auto max-w-[360px] object-contain"
+      src="/images/dulce-bebe-logo-v6.svg"
+      className="h-14 sm:h-16 lg:h-20 w-auto max-w-[420px] object-contain"
       alt="Dulce Bebé"
       loading="eager"
     />
