c9f5228a15f2a2e4c4c28310e9dfc9139e3bbdcc
federico beron <federicoberon@outlook.com>
2026-08-22 22:23:40 -0300

    style: tighten Dulce Bebe logo spacing in header

--- components/BrandLogo.js
+++ components/BrandLogo.js
@@ -1,11 +1,13 @@
 function BrandLogo() {
   return (
-    <img
-      src="/images/dulce-bebe-logo-v6.svg"
-      className="h-14 sm:h-16 lg:h-20 w-auto max-w-[420px] object-contain"
-      alt="Dulce Bebé"
-      loading="eager"
-    />
+    <div className="flex items-center py-1 sm:py-1.5 lg:py-2">
+      <img
+        src="/images/dulce-bebe-logo-v6.svg"
+        className="block h-12 sm:h-14 lg:h-16 w-auto max-w-[360px] object-contain"
+        alt="Dulce Bebé"
+        loading="eager"
+      />
+    </div>
   );
 }
 
