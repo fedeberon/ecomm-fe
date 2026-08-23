9bc017cc16f10bd93cbe673d365460f695e49924
federico beron <federicoberon@outlook.com>
2026-08-22 21:53:32 -0300

    feat: add dedicated BrandLogo component

---
+++ components/BrandLogo.js
@@ -0,0 +1,12 @@
+function BrandLogo() {
+  return (
+    <img
+      src="/images/dulce-bebe-logo-v4.svg"
+      className="h-14 sm:h-16 lg:h-20 w-auto max-w-[360px] object-contain"
+      alt="Dulce Bebé"
+      loading="eager"
+    />
+  );
+}
+
+export default BrandLogo;
